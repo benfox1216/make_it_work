1eca54fcc6be33d0cdcf40c9869ad71837fec038
Ben Fox <benfox1216@gmail.com>
2020-02-07 10:04:42 -0700

    Add test and code to make it pass for User Story 1

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
   def show
-    
+    @project = Project.find(params[:project_id])
+    @challenge = Challenge.find(params[:challenge_id])
   end
 end
--- spec/features/project/show_spec.rb
+++ spec/features/project/show_spec.rb
@@ -3,14 +3,16 @@ require 'rails_helper'
 describe "As a visitor" do
   describe "when I visit the project's show page" do
     before :each do
-      furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
-      furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+      @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+      @project_1 = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
       
-      visit '/challenges/:challenge_id/projects/:project_id'
+      visit "/challenges/#{@furniture_challenge.id}/projects/#{@project_1.id}"
     end
     
     it "I see the project's name, material, and theme" do
-      
+      expect(page).to have_content(@project_1.name)
+      expect(page).to have_content(@project_1.material)
+      expect(page).to have_content(@furniture_challenge.theme)
     end
   end
 end
