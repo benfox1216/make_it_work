36f18c433cf70d672b71d4d93cea9f5f7de0c492
Ben Fox <benfox1216@gmail.com>
2020-02-07 09:53:41 -0700

    Set up test for US 1, and the file structure/code to visit the show page

--- spec/features/project/show_spec.rb
+++ spec/features/project/show_spec.rb
@@ -1,3 +1,16 @@
 require 'rails_helper'
 
-describe 
+describe "As a visitor" do
+  describe "when I visit the project's show page" do
+    before :each do
+      furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+      furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+      
+      visit '/challenges/:challenge_id/projects/:project_id'
+    end
+    
+    it "I see the project's name, material, and theme" do
+      
+    end
+  end
+end
